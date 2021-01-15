5edbc028e5bb32f0c80f993b194e5767a8ae5fb4
Levi Kohler <kohler.la01@gmail.com>
2021-01-15 08:56:21 -0800

    use console to find bug, script.js in head instead of scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -2,8 +2,8 @@ $(document).ready(function() {
   const fun = confirm ("Do you like to have fun? Click OK for yes or Cancel for no.");
 
   if (fun) {
-    $("#coder").show();
-  } else {
-    ("#not-a-coder").show();
+    $('#coder').show();
+  }else {
+    ('#not-a-coder').show();
   }
 });

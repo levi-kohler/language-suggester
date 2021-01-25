56f3bec14d2b92e52d5a44f60652eb7b70539174
Levi Kohler <kohler.la01@gmail.com>
2021-01-24 17:08:16 -0800

    add hide to the answers id so only the corrent answer will be shown even if submit is pushed again

--- js/scripts.js
+++ js/scripts.js
@@ -2,18 +2,6 @@ function totalValue(number1, number2, number3, number4, number5) {
   return number1 + number2 + number3 + number4 + number5;
 }
 
-function Javascript() {
-  return "Javascript! Often abbreviated as JS, Javascript is a programming language that conforms to the ECMAScript specification. JavaScript is high-level, often just-in-time compiled, and multi-paradigm. It has curly-bracket syntax, dynamic typing, prototype-based object-orientation, and first-class functions."
-}
-
-function Csharp() {
-  return "C#! C# (pronounced 'C-sharp') is an object-oriented programming language from Microsoft that aims to combine the computing power of C++ with the programming ease of Visual Basic. C# is based on C++ and contains features similar to those of Java. C# is designed to work with Microsoft's . Net platform."
-}
-
-// function Ruby() {
-//   return "Ruby! Ruby is A dynamic, open source programming language with a focus on simplicity and productivity. It has an elegant syntax that is natural to read and easy to write."
-// }
-
 $(document).ready(function() {
   const fun = confirm ("Do you like to have fun? Click OK for yes or Cancel for no.");
 
@@ -31,15 +19,21 @@ $(document).ready(function() {
     let number4 = parseInt($("#dessert").val());
     let number5 = parseInt($("#climate").val());
     let total = totalValue (number1, number2, number3, number4, number5)
-    let result;
+    // let result;
     if (total > 5) {
       $("#ruby").show();
+      $("#csharp").hide();
+      $("#javascript").hide();
 
     } else if (total < 5) {
       $("#csharp").show();
+      $("#ruby").hide();
+      $("#javascript").hide();
 
     } else {
       $("#javascript").show();
+      $("#csharp").hide();
+      $("#ruby").hide();
     }
   });
 });
